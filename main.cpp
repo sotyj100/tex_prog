2101a28335f24bc4245ff9ec79db4cffae12179f
sotyj100 <sotyj100@yandex.ru>
2016-12-11 20:47:44 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
+#include "head.h"
+#include <string>
 #include <iostream>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello world!" << endl;
+    string maska;
+    F_Poisk* fff = new F_Poisk();
+
+    if (argc > 1)
+        maska = argv[1];
+    else
+    {
+        cout << "Vvod disk, catalog, and/or maska faila ([drive:][ras]maska): ";
+        cin >> maska;
+        cout << endl;
+        if (!maska.length())
+            return 1;
+    }
+
+    fff->setmaska(maska);
+
+    while (fff->hasMore())
+        fff->next()->show();
+
     return 0;
 }
