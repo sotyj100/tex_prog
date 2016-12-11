3e085b73106b9faa2fa36b80529b2587d1b6d69b
sotyj100 <sotyj100@yandex.ru>
2016-12-11 15:52:02 +0400

    Add files via upload

--- rabf.cpp
+++ rabf.cpp
@@ -1,5 +1,6 @@
 #include "head.h"
 #include <string>
+
 #include <iostream>
 #include <io.h>
 using namespace std;
@@ -43,12 +44,12 @@ for (int i = 0; i < mask.length(); i++)
 }
 FIter* FIter_nn::next()
 {
-    FileItem* res = cache;
+    FIter* res = cache;
     cache = NULL;
     return res;
 }
 
-void FileIterator::setFileMask(const string& mask)
+void FIter_nn::setFileMask(const string& mask)
 {
     fileMask = mask;
 }
@@ -60,4 +61,56 @@ bool FIter_nn::hasMore()
         return true;
     else
         return false;
+}
+
+
+
+FIter* FIter_nn::search(const string& fileMask)
+{
+    _finddata_t FindData;
+
+    string defFileMask = fileMask.substr(0, fileMask.find_last_of('\\') + 1) + "*.*";
+    if (FindHandle == 0)
+        FindHandle = _findfirst(defFileMask.c_str(), &FindData);
+    while (_findnext(FindHandle, &FindData) != -1L)
+    {
+        if ((FindData.name == string(".")) || (FindData.name == string("..")))
+        {
+            continue;
+        }
+        if (IsDirectory(FindData))
+        {
+            string newFileMask = fileMask;
+            newFileMask.insert(fileMask.find_last_of('\\') + 1, string(FindData.name) + '\\');
+            this->subIterator = new FIter_nn(newFileMask);
+            while (this->subIterator->hasMore())
+                this->subIterator->next()->show();
+        }
+        if (compareToMask(fileMask.substr(fileMask.find_last_of('\\') + 1), string(FindData.name)))
+        {
+            string name = string(FindData.name);
+            string path = defFileMask.substr(0, defFileMask.find_last_of('\\'));
+            if (path == "*.*")
+                path = "root";
+            cache = new FIter(name, path);
+            return cache;
+        }
+    }
+    _findclose(FindHandle);
+        return NULL;
+}
+
+
+FIter::FIter() {};
+
+FIter::FIter(const string& name, const string& path)
+{
+    this->name = name;
+    this->path = path;
+}
+
+void FIter::show()
+{
+    cout << "File :" << name << endl;
+    cout << "In catalog: " << path << endl << endl;
 }
