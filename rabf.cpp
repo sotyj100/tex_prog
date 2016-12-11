aade2162a3c181d71a538b81202da58af7fa426f
sotyj100 <sotyj100@yandex.ru>
2016-12-11 20:04:33 +0400

    Add files via upload

--- rabf.cpp
+++ rabf.cpp
@@ -10,6 +10,37 @@ F_Poisk::F_Poisk(const string& maska)
     FindHandle = 0;
 }
 
+
+inline bool F_Poisk::IsDirectory(const _finddata_t &FindData)
+    {
+        return ((FindData.attrib & _A_SUBDIR) != 0);
+    }
+bool F_Poisk::compareToMask(const string& mask, const string& file)
+{
+    int z = 0;
+    for (int i = 0; i < mask.length(); i++)
+        for (int j = z; j < file.length(); j++)
+        {
+            z = j;
+            if (mask[i] == '*')
+            {
+                if (file[j] == '.')
+                {
+                    break;
+                }
+                else
+                    continue;
+            }
+            else if ((mask[i] == '?') || (mask[i] == file[j]))
+            {
+                z++;
+                break;
+            }
+            return false;
+        }
+    return true;
+    }
+
 void F_Poisk::setmaska(const string& mask)
 {
     maska = mask;
