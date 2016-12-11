de3dabe66dc3469baa2dd6a163b3f9d50d178999
sotyj100 <sotyj100@yandex.ru>
2016-12-11 11:20:35 +0400

    Update head.h

--- head.h
+++ head.h
@@ -1,6 +1,8 @@
 #include <string>
 #include <io.h>
+
 using namespace std;
+
 class FIter
 {
     std::string name;
@@ -10,6 +12,7 @@ public:
     FIter(const std::string& name, const std::string& path);
     void show();
 };
+
 
 class FIter_nn
 {
@@ -24,8 +27,11 @@ class FIter_nn
     inline bool IsDirectory(const _finddata_t &FindData);
 bool compareToMask(const string& mask, const string& file);
 
-}
+public:
+    FileIterator() : FindHandle(NULL) {};
+    FileIterator(const string& fileMask);
+    void setFileMask(const string& mask);
+    bool hasMore();
+    FileItem* next();
 
-
-:
-;
+};
