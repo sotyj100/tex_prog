c31f4e404e2d0ab7b605d93111e9c5af61b9bdd6
sotyj100 <sotyj100@yandex.ru>
2016-12-11 13:20:45 +0400

    Add files via upload

--- rabf.cpp
+++ rabf.cpp
@@ -1,1 +1,20 @@
+#include "head.h"
+#include <string>
+#include <iostream>
+#include <io.h>
+using namespace std;
 
+FIter_nn::FIter_nn(const string& fileMask)
+{
+    this->fileMask = fileMask;
+    FindHandle = 0;
+}
+
+inline bool FIter_nn::IsDirectory(const _finddata_t &FindData)
+    {
+        return ((FindData.attrib & _A_SUBDIR) != 0);
+}
+
+bool FIter_nn::compareToMask(const string& mask, const string& file)
+{
+int pos = 0;
